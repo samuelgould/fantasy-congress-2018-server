7349a7803e1e85f11655c7ee87055bfd2a60ce85
Sam Gould <sjg326@gmail.com>
2018-06-07 15:49:28 -0700

    updated user schema to include teamSubmitted boolean property for when a user submits their team

--- models.js
+++ models.js
@@ -23,9 +23,10 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  firstName: {type: String, default: ''},
-  lastName: {type: String, default: ''},
+  firstName: { type: String, default: '' },
+  lastName: { type: String, default: '' },
   teamName: { type: String, required: true },
+  teamSubmitted: { type: Boolean, default: false },
   senate: [{
     candidate_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Candidate' }
   }],
